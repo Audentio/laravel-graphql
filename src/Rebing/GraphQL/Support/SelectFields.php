686b45687cc3fff43f78fd77bdbd2a60dc0005f9
Jake B <jake.booher@gmail.com>
2021-08-11 14:54:15 -0500

    Update SelectFields.php

--- src/Rebing/GraphQL/Support/SelectFields.php
+++ src/Rebing/GraphQL/Support/SelectFields.php
@@ -6,13 +6,53 @@ use Closure;
 use GraphQL\Error\InvariantViolation;
 use GraphQL\Type\Definition\FieldDefinition;
 use GraphQL\Type\Definition\Type as GraphqlType;
+use GraphQL\Type\Definition\WrappingType;
 use Rebing\GraphQL\Support\SelectFields as SelectFieldsBase;
 
 class SelectFields extends SelectFieldsBase
 {
+    public static function getSelectableFieldsAndRelations(array $queryArgs, array $requestedFields, GraphqlType $parentType, ?Closure $customQuery = null, bool $topLevel = true, $ctx = null)
+    {
+        $select = [];
+        $with = [];
+
+        if ($parentType instanceof WrappingType) {
+            $parentType = $parentType->getWrappedType(true);
+        }
+        $parentTable = static::getTableNameFromParentType($parentType);
+        $primaryKey = static::getPrimaryKeyFromParentType($parentType);
+
+        static::handleFields($queryArgs, $requestedFields, $parentType, $select, $with, $ctx);
+
+        // If a primary key is given, but not in the selects, add it
+        if (null !== $primaryKey) {
+            $primaryKey = $parentTable ? ($parentTable . '.' . $primaryKey) : $primaryKey;
+            $alternateKey = $parentTable ? ($parentTable . '.' . '*') : '*';
+
+            if (!in_array($primaryKey, $select) && !in_array($alternateKey, $select)) {
+                $select[] = $primaryKey;
+            }
+        }
+
+        if ($topLevel) {
+            return [$select, $with];
+        }
+
+        return function ($query) use ($with, $select, $customQuery, $requestedFields, $ctx): void {
+            if ($customQuery) {
+                $query = $customQuery($requestedFields['args'], $query, $ctx);
+            }
+
+            $query->select($select);
+            $query->with($with);
+        };
+    }
+
     protected static function handleFields(array $queryArgs, array $requestedFields, GraphqlType $parentType, array &$select, array &$with, $ctx): void
     {
         parent::handleFields($queryArgs, $requestedFields, $parentType, $select, $with, $ctx);
+        $parentTable = static::getTableNameFromParentType($parentType);
+        $select = [$parentTable ? ($parentTable . '.' . '*') : '*'];
 
         foreach ($requestedFields['fields'] as $key => $field) {
             // Ignore __typename, as it's a special case
