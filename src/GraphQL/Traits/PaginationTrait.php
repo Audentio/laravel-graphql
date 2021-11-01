74633647257613255b13242b1138e309c4bd1e0c
Jake Booher <jake.booher@gmail.com>
2021-11-01 13:37:28 -0500

    Adds better pagination support

--- src/GraphQL/Traits/PaginationTrait.php
+++ src/GraphQL/Traits/PaginationTrait.php
@@ -9,9 +9,9 @@ use Illuminate\Pagination\LengthAwarePaginator;
 
 trait PaginationTrait
 {
-    public static function addPaginationArgs($scope, array &$args)
+    public static function addPaginationArgs($scope, array &$args, ?string $parentObject = null): void
     {
-        $args['paginate'] = [
+        $paginate = [
             'description' => 'Pagination information',
             'type' => new InputObjectType([
                 'name' => 'Paginate' . $scope,
@@ -27,18 +27,37 @@ trait PaginationTrait
                 ],
             ]),
         ];
+
+        if ($parentObject) {
+            $config = [
+                'name' => 'Paginate' . $scope . ucfirst($parentObject),
+                'fields' => [],
+            ];
+            if (array_key_exists($parentObject, $args)) {
+                $config = $args[$parentObject]->config;
+            }
+
+            $config['fields']['paginate'] = $paginate;
+            $args[$parentObject] = new InputObjectType($config);
+        } else {
+            $args['paginate'] = $paginate;
+        }
     }
 
-    protected static function buildPaginationParams(array $args, $perPage = 20, $maxPerPage = 50): array
+    protected static function buildPaginationParams(array $args, $perPage = 20, $maxPerPage = 50, ?string $parentObject = null): array
     {
+        $argObject = $args;
+        if ($parentObject) {
+            $argObject = $args[$parentObject];
+        }
         $page = 1;
-        if (!empty($args['paginate']['page'])) {
-            $page = $args['paginate']['page'];
+        if (!empty($argObject['paginate']['page'])) {
+            $page = $argObject['paginate']['page'];
         }
         $page = max(1, $page);
 
-        if (!empty($args['paginate']['perPage'])) {
-            $perPage = $args['paginate']['perPage'];
+        if (!empty($argObject['paginate']['perPage'])) {
+            $perPage = $argObject['paginate']['perPage'];
         }
         $perPage = min($perPage, $maxPerPage);
 
@@ -48,9 +67,9 @@ trait PaginationTrait
         ];
     }
 
-    public static function paginateResults($root, array $args, $perPage=20, $maxPerPage=50): ?LengthAwarePaginator
+    public static function paginateResults($root, array $args, $perPage=20, $maxPerPage=50, ?string $parentObject = null): ?LengthAwarePaginator
     {
-        list($page, $perPage) = self::buildPaginationParams($args, $perPage, $maxPerPage);
+        list($page, $perPage) = self::buildPaginationParams($args, $perPage, $maxPerPage, $parentObject);
 
         return $root->paginate($perPage, ['*'], 'page', $page);
     }
