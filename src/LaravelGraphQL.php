83e148999b86c47c751e7d1415d90395a27b1273
Lukas Wieditz <lukasw@audentio.com>
2025-03-25 11:32:53 +0000

    Fix typo in constant

--- src/LaravelGraphQL.php
+++ src/LaravelGraphQL.php
@@ -16,7 +16,7 @@ class LaravelGraphQL
 {
     const ERR_PERMISSION = 'permission';
     const ERR_NOT_FOUND = 'notFound';
-    const ERR_VALIDAATION = 'validation';
+    const ERR_VALIDATION = 'validation';
     const ERR_INVALID_PARAMETER = 'invalidParameter';
 
     const QUERIES_EXECUTED_DEBUG_IDENTIFIER = 'audentioLaravelGraphQLQueriesExecuted';
